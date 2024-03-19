93bc95706df94cd89690a0b19826a13f8c0386c6
Arturo <arturo.veliz@ucb.edu.bo>
2024-03-18 22:10:18 -0400

    Prueba de calculo de puntaje de 20 lanzamientos con 1 pinos derribados en cada lanzamiento

--- src/juego.js
+++ src/juego.js
@@ -1,12 +1,15 @@
-class juegoBolos
-{
-
-    lanzamiento(pinos)
-    {
+class juegoBolos {
+    constructor() {
+        this.puntaje = 0; // Inicialización del atributo puntaje en el constructor
     }
-    getPuntaje()
-    {
-        return 0;
+
+    lanzamiento(pinos) {
+        this.puntaje += pinos; // Incrementar el puntaje con el valor de los pinos
+    }
+
+    getPuntaje() {
+        return this.puntaje; // Devolver el puntaje actual
     }
 }
+
 module.exports = juegoBolos;
--- src/juego.test.js
+++ src/juego.test.js
@@ -2,13 +2,13 @@ const juegoBolos = require('./juego.js'); // Importar la clase desde el archivo 
 
 describe("Juego De Bolos", () => {
   it("Iniciar clase Juego", () => {
-    const juego = new juegoBolos(); // Crear una instancia de la clase 'juegoBolos'
+    let juego = new juegoBolos(); // Crear una instancia de la clase 'juegoBolos'
     juego.lanzamiento(0)
     expect(juego.getPuntaje()).toEqual(0);
   });
 
   it("Deberia calcular puntaje de los 20 lanzamientos", () => {
-    const juego = new juegoBolos(); // Crear una instancia de la clase 'juegoBolos'
+    let juego = new juegoBolos(); // Crear una instancia de la clase 'juegoBolos'
     for(let i=0;i<20;i++)
     {
       juego.lanzamiento(0)
@@ -16,4 +16,13 @@ describe("Juego De Bolos", () => {
     expect(juego.getPuntaje()).toEqual(0);
   });
 
+
+  it("Deberia calcular puntaje de los 20 lanzamientos con 1 pino derribado", () => {
+    let juego = new juegoBolos(); // Crear una instancia de la clase 'juegoBolos'
+    for(let i=0;i<20;i++)
+    {
+      juego.lanzamiento(1)
+    }
+    expect(juego.getPuntaje()).toEqual(20);
+  });
 });
