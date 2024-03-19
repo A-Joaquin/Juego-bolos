14f8137351ee0e5e86ad3df6df548965f5ab10b6
Arturo <arturo.veliz@ucb.edu.bo>
2024-03-18 22:05:01 -0400

    Prueba del calculos de puntaje de 20 lanzamientos con 0 pinos CORREGIDO

--- src/juego.test.js
+++ src/juego.test.js
@@ -7,9 +7,12 @@ describe("Juego De Bolos", () => {
     expect(juego.getPuntaje()).toEqual(0);
   });
 
-  it("Deberia calcular los 20 lanzamientos", () => {
+  it("Deberia calcular puntaje de los 20 lanzamientos", () => {
     const juego = new juegoBolos(); // Crear una instancia de la clase 'juegoBolos'
-    juego.lanzamiento(0)
+    for(let i=0;i<20;i++)
+    {
+      juego.lanzamiento(0)
+    }
     expect(juego.getPuntaje()).toEqual(0);
   });
 
